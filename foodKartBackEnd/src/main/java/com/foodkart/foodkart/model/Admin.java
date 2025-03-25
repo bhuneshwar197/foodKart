ecfbf99ce6733087b3986c59f0b34e7bfc3e355a
Bhuneshwar Kumar <bhuneshwar197@gmail.com>
2025-03-25 22:44:16 +0530

    added admin operation

--- foodKartBackEnd/src/main/java/com/foodkart/foodkart/model/Admin.java
+++ foodKartBackEnd/src/main/java/com/foodkart/foodkart/model/Admin.java
@@ -1,2 +1,30 @@
-package com.foodkart.foodkart.model;public class Admin {
+package com.foodkart.foodkart.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author Simpson Alfred
+ */
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String name;
+    private String mobile;
+    private String password;
 }
+
--- foodKartBackEnd/src/main/java/com/foodkart/foodkart/repository/AdminRepository.java
+++ foodKartBackEnd/src/main/java/com/foodkart/foodkart/repository/AdminRepository.java
@@ -1,2 +1,8 @@
-package com.foodkart.foodkart.repository;public class AdminRepository {
+package com.foodkart.foodkart.repository;
+
+import com.foodkart.foodkart.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Admin findByEmail(String email);
 }
